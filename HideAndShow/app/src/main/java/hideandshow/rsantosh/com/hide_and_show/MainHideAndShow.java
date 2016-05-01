1d5eed3279e1598c0184bbec00e5b87df5a6a0fc
santoshr1982 <santoshr1982@yahoo.com>
2016-05-01 23:45:17 +0530

    Hide and show image in text view
    This program has been written to hide and show the image in text view.

---
+++ HideAndShow/app/src/main/java/hideandshow/rsantosh/com/hide_and_show/MainHideAndShow.java
@@ -0,0 +1,17 @@
+package hideandshow.rsantosh.com.hide_and_show;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainHideAndShow extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hide_and_show);
+    }
+}
